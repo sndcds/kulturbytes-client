da4f9c2256ab4660a8dab23072869882b9948ade
Roald Christesen <roald@grain.one>
2026-07-27 09:02:59 +0200

    new favicon inclandroid andapple icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,13 +5,21 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
         {
           rel: 'preload',
           href: '/fonts/Albert_Sans/AlbertSans-VariableFont_wght.ttf',
           as: 'font',
           type: 'font/ttf',
           crossorigin: 'anonymous'
-        }
+        },
+      ],
+      meta: [
+        { name: 'theme-color', content: '#ffffff' }
       ]
     }
   },
