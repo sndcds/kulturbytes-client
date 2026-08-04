ba0e605cde3999504a093869d40bb3f0343be329
Roald Christesen <roald@grain.one>
2026-08-04 13:49:53 +0200

    add default head in map layout

--- app/layouts/map.vue
+++ app/layouts/map.vue
@@ -14,8 +14,45 @@
   </div>
 </template>
 
-<style scoped lang="scss">
 
+<script setup lang="ts">
+const config = useRuntimeConfig()
+
+useHead({
+  script: [
+    {
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify({
+        "@context": "https://schema.org",
+        "@graph": [
+          {
+            "@type": "Organization",
+            "@id": `${config.public.siteUrl}/#organization`,
+            "name": "DatenSindDaten e. V.",
+            "url": config.public.siteUrl,
+            "logo": {
+              "@type": "ImageObject",
+              "url": `${config.public.siteUrl}/images/dsd-logo-typo-black.svg`
+            }
+          },
+          {
+            "@type": "WebSite",
+            "@id": `${config.public.siteUrl}/#website`,
+            "name": "kulturbytes",
+            "url": config.public.siteUrl,
+            "publisher": {
+              "@id": `${config.public.siteUrl}/#organization`
+            }
+          }
+        ]
+      })
+    }
+  ]
+})
+</script>
+
+
+<style scoped lang="scss">
 .layout {
   width: 100%;
   min-height: 100vh;
